34d1b819bf0489d2bffb34948cfeef0b6890fa09
James Tyack <jamestyack@gmail.com>
2017-07-04 23:33:25 -0700

    Added auth example

--- web/router.ex
+++ web/router.ex
@@ -7,6 +7,7 @@ defmodule Discuss.Router do
     plug :fetch_flash
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug Discuss.Plugs.SetUser
   end
 
   pipeline :api do
@@ -16,16 +17,17 @@ defmodule Discuss.Router do
   scope "/", Discuss do
     pipe_through :browser # Use the default browser stack
 
-    
-
     resources "/", TopicController
-    # get "/", TopicController, :index
-    # get "/topics/new", TopicController, :new
-    # post "/topics", TopicController, :create
-    # get "/topics/:id/edit", TopicController, :edit
-    # post "/topics/:id", TopicController, :update
-    # delete "/topics/:id", TopicController, :delete
   end
+
+  scope "/auth", Discuss do
+    pipe_through :browser
+
+    get "/signout", AuthController, :signout
+    get "/:provider", AuthController, :request
+    get "/:provider/callback", AuthController, :callback
+end
+  
 
   # Other scopes may use custom stacks.
   # scope "/api", Discuss do
